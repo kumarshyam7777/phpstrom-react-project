a59d630c34e58b8da34dcbedb77d2c9fa7325194
kumarshyam7777 <kumar.shyam7777@gmail.com>
2020-10-03 16:31:44 +0530

    Initial commit rest api

---
+++ src/components/FunctionComponent.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+const FunctionComponent = (props) => {
+    const [email,setEmail] = useState();
+
+    return(
+        <div>
+
+
+        </div>
+    )
+}
+export default FunctionComponent
